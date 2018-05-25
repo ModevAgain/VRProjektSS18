d6d5a444b2f4b417bc468db7bb25dde8df6238cb
Nicolai Scham <nicolai@studio-fizbin.de>
2018-05-25 14:06:46 +0200

    - added target prefab - added gun prefab

---
+++ Assets/Target.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+    TargetController targetCon;
+
+	// Use this for initialization
+	void Start () {
+        targetCon = GetComponentInParent<TargetController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Bullet")
+        {
+            targetCon.DestroyTarget(gameObject);
+        }     
+    }
+}
---
+++ Assets/TargetController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour {
+
+    public GameObject Target;
+
+    private bool _firstStart = true;
+
+    private List<GameObject> _targets = new List<GameObject>();
+
+    private WaitForSeconds _targetRespawnTime;
+
+	// Use this for initialization
+	void Start () {
+        _targetRespawnTime = new WaitForSeconds(5);
+        spawnTargets();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void spawnTargets()
+    {
+        if (_firstStart)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                GameObject tempTarget = Instantiate(Target);
+                tempTarget.transform.position = transform.position;
+
+                _targets.Add(tempTarget);
+            }
+            _firstStart = false;
+        }
+        else
+        {
+            GameObject tempTarget = Instantiate(Target);
+            tempTarget.transform.position = transform.position;
+
+            _targets.Add(tempTarget);
+        }
+    }
+
+    private IEnumerator RespawnTarget()
+    {
+        yield return _targetRespawnTime;
+
+        spawnTargets();        
+    }
+
+    public void DestroyTarget(GameObject target)
+    {
+        _targets.Remove(target);
+        Destroy(target);
+        StartCoroutine(RespawnTarget());
+    }
+}
