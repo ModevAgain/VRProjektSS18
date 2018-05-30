bd3575fa59daea5cc5412033f46497701cd372db
DESKTOP-5O082G9\Moritz Reinert <mreinert@gmx.net>
2018-05-30 13:15:03 +0200

    Probuilder, PostProcessingStack importiert, RoomTemplates angefangen

--- Assets/_Project/Scripts/DoorScript.cs
+++ Assets/_Project/Scripts/DoorScript.cs
@@ -22,12 +22,14 @@ public class DoorScript : MonoBehaviour {
 
     public void OpenDoor()
     {
+
         if (_isOpen)
             return;
 
+        Debug.Log("gunzt");
         _isOpen = true;
 
-        transform.DOLocalMoveY(transform.localPosition.y - Y_Offset, 0.5f);
+        transform.DOLocalMoveY(transform.localPosition.y - Y_Offset, 0.5f).OnComplete(() => Debug.Log("finished"));
     }
 
 }
