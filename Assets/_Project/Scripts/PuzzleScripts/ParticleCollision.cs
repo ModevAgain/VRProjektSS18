8e9dcc7bfdad09224ec48376286bbfa22fad0e40
Nicolai Scham <nicolai@studio-fizbin.de>
2018-06-14 01:31:41 +0200

    - LeverGunGame

---
+++ Assets/_Project/Scripts/PuzzleScripts/ParticleCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCollision : MonoBehaviour
+{
+    private ParticleSystem _ps;
+    List<ParticleSystem.Particle> enter = new List<ParticleSystem.Particle>();
+
+
+    public delegate void ParticleReceiver();
+    public ParticleReceiver ParticleReceived;
+
+    private void Start()
+    {
+        _ps = GetComponent<ParticleSystem>();
+    }
+
+    void OnParticleTrigger()
+    {       
+        // get the particles which matched the trigger conditions this frame
+        int numEnter = _ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+
+        if (numEnter > 0)
+        {
+            Debug.Log("yooo");
+
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/RoomScripts/Levels/LeverGunGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverGunGame : MonoBehaviour {
+
+    private DoorActivation _door;
+    private ParticleCollision _particleCollision;
+
+
+	// Use this for initialization
+	void Start () {
+        _door = GetComponentInChildren<DoorActivation>();
+        _particleCollision.ParticleReceived = LevelEnd;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LevelEnd()
+    {
+        _door.OpenDoorFromButton();
+    }
+}
