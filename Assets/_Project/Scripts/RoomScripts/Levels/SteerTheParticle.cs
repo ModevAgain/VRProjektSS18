9fd753eb4f20e7c445726260f4f69c28e8defc5d
DESKTOP-5O082G9\Moritz Reinert <mreinert@gmx.net>
2018-06-13 19:17:20 +0200

    SteerTheParticle Level erstellt

---
+++ Assets/_Project/Scripts/RoomScripts/Levels/SteerTheParticle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteerTheParticle : ContentScript {
+
+    public ParticleTrigger PTrigger;
+
+	// Use this for initialization
+	void Start () {
+
+        PTrigger.Finished = Finish;
+
+	}
+
+
+    public void Finish()
+    {
+        Door.OpenDoorFromButton();
+    }
+
+}
