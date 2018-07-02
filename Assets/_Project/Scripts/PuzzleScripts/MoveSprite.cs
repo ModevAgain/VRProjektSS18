42cc329dc7108c54aefc78bd6607ecf9c7535ce4
DESKTOP-5O082G9\Moritz Reinert <mreinert@gmx.net>
2018-07-02 13:17:02 +0200

    MoveSprite eingebaut

---
+++ Assets/_Project/Scripts/PuzzleScripts/MoveSprite.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MoveSprite : MonoBehaviour
+{
+
+    public Vector3 DesiredPos;
+
+    public void Move()
+    {
+        transform.DOLocalMove(DesiredPos, 0.5f);
+    }
+
+}
