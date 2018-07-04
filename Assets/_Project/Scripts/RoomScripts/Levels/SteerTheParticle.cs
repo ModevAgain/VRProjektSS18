8b3cd638d15bd1751881b15816dadd3a76c9bfed
Nicolai Scham <nicolai@studio-fizbin.de>
2018-07-04 10:33:37 +0200

    - added Particle Receiver to SteerTheParticle and ParticleGame - implemented receiver logic to SteerTheParticle

--- Assets/_Project/Scripts/RoomScripts/Levels/SteerTheParticle.cs
+++ Assets/_Project/Scripts/RoomScripts/Levels/SteerTheParticle.cs
@@ -1,29 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SteerTheParticle : ContentScript {
 
     public ParticleCollision PCollision;
     public Collider ParticleKiller;
     private bool _doorOpened;
+    public GameObject ParticleReceiver;
+    private Material _receiverMat;
+    private bool _dissolved = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
+        _receiverMat = ParticleReceiver.GetComponent<MeshRenderer>().material;
         PCollision.ParticleReceived += Finish;
         ParticleKiller.enabled = false;
-
     }
 
 
     public void Finish()
     {
-        if (!_doorOpened)
+        StartCoroutine(DissolveReceiver());
+        
+        if (!_doorOpened && _dissolved)
         {
             _doorOpened = true;
             Door.OpenDoorFromButton();
             ParticleKiller.enabled = true;
+        }
+    }
+
+    private IEnumerator DissolveReceiver()
+    {
+        if (!_dissolved)
+        {
+            _dissolved = true;
+
+            Tween _dissolveRec = _receiverMat.DOFloat(1, "_SliceAmount", 2);
+
+            yield return _dissolveRec.WaitForCompletion();
+
+            Finish();
         }
     }
 
